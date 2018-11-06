6c50fc9d8b5e289d2e6014a9f9fc4fc2b7613d2e
Jshhhh <36183898+Jshhhh@users.noreply.github.com>
2018-11-05 16:42:17 -0800

    Moves players and editing name to redux state

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,14 +1,30 @@
-
+import { ADD_PLAYER, CHANGE_NAME } from '../actions/playerActions';
 
 const initialState = {
-    players: [],
+    players: ['Player1', 'Player2', 'Player3'],
+    playerCount: 3,
     heroes: ['Torm', 'Sir Moo', 'Goldfinga', 'Brockenstock', 'Murka Mistcleaver', 'Lady Mary', 'Lanky Lowshot', 'Aristide'],
 };
 
 const rootReducer = (state=initialState, action) => {
     switch (action.type) {
-        case 'CHANGE_NAME':
-            return state;
+        case ADD_PLAYER:
+            let newPlayerCount = state.playerCount + 1;
+            if (newPlayerCount < 7) {
+                return {
+                    ...state,
+                    players: [...state.players, 'Player' + newPlayerCount],
+                    playerCount: newPlayerCount
+                };
+            } else return state;
+        case CHANGE_NAME:
+            let newPlayerName = state.players;
+            console.log(state.players, action.payload)
+            newPlayerName[action.payload.id] = action.payload.name;
+            return {
+                ...state,
+                players: newPlayerName
+            }
         default:
             return state;
     }
