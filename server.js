d416653594d3c3b5d1b7c7685bb88bc2fd5bd62f
Jshhhh <36183898+Jshhhh@users.noreply.github.com>
2018-12-12 17:34:31 -0800

    Adds config params to axios request

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
-
+app.use(express.json());
 app.use(express.static(path.join(__dirname, '/dist')));
 
 app.post('/submit', (req, res) => {
   console.log(req.body);
+  res.end();
 });
 
 const port = 3333;
