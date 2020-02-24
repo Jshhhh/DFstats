8c964f2d014e5cee435613427c01e7e8533c3ef4
Jshhhh <36183898+Jshhhh@users.noreply.github.com>
2020-02-24 15:19:46 -0800

    Add react select package

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Select from 'react-select';
 
 import Players from './Players';
 import LineGraph from './LineGraph';
@@ -9,6 +10,7 @@ const App = () => (
     <nav>
       <h1 className="header">DF Stats</h1>
     </nav>
+    <Select options={[{ value: '1', label: '1' }, { value: 'abadasfdfasdf', label: 'adsfasdfasdfadsf' }]} />
     <section className="ui-sec">
       <Players />
       <LineGraph />
